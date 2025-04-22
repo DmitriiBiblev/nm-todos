aa3f6258eb406f21f22f10f72fab428fe16461bb
Dmitrii Biblev <biblev1608@gmail.com>
2025-04-22 21:14:11 +0200

    created header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NAV_ITEMS } from './data';
+import { NavItem } from './interfaces';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'nm-todos';
+  protected readonly navItems: NavItem[] = NAV_ITEMS;
 }
---
+++ src/app/data/nav-items.data.ts
@@ -0,0 +1,7 @@
+import { NavItem } from '../interfaces';
+
+export const NAV_ITEMS: NavItem[] = [
+  { title: 'Add', path: 'add' },
+  { title: 'List', path: 'list' },
+  { title: 'Favorite', path: 'favorite' }
+];
